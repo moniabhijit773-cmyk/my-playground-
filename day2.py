01fcd774c6507ef0588fa3925f4c1c504e4cc73d
abhijit-algo <moniabhijit773@gmail.com>
2026-06-14 01:43:35 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,12 @@
+# Day 3: Checking Odd or Even Number in Python
+
+print("--- Even or Odd Number Checker ---")
+
+# Take number input from the user
+number = int(input("Enter any number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print("This is an EVEN number.")
+else:
+    print("This is an ODD number.")
